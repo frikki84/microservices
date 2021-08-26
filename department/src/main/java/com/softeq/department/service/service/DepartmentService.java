4e66c620734950c30da0b2b3f5f4072d279f915b
ydziadkouskaya <s6Begikira2010*>
2021-08-26 18:48:09 +0300

    First Init

---
+++ department/src/main/java/com/softeq/department/service/service/DepartmentService.java
@@ -0,0 +1,24 @@
+package com.softeq.department.service.service;
+
+import com.softeq.department.service.entity.Department;
+import com.softeq.department.service.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DepartmentService {
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findDepartmentById(departmentId);
+    }
+}
---
+++ user-service/src/main/java/com/softeq/user/service/value_object/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.softeq.user.service.value_object;
+
+import com.softeq.user.service.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
